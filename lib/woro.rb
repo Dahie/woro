576fcc185ec96ae752faa90d0bb351cbb64e00b3
Daniel Senff <mail@danielsenff.de>
2015-07-01 07:27:52 +0200

    require all installed gems, that prefix 'woro-' by default minor change to mina's task executation

--- lib/woro.rb
+++ lib/woro.rb
@@ -4,3 +4,11 @@ require 'woro/task'
 require 'woro/adapters/base'
 require 'woro/adapters/ftp'
 require 'woro/configuration'
+
+# require any gem with the prefix "woro" found in the gemset
+[].tap do |woro_specs|
+  Gem::Specification::each do |spec|
+    woro_specs << spec.name if spec.name.start_with?('woro') && spec.name != 'woro'
+  end
+  woro_specs.uniq.each { |spec| require spec }
+end
